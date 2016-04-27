06e9f53e4a94a221b4c8672dbdd7068a737d5fca
Wilfried Boos <wilfriedb@users.noreply.github.com>
2016-04-27 12:20:38 +0200

    Added a .nuspec file

--- Types/Properties/AssemblyInfo.cs
+++ Types/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Types")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Add an Option type like F#'s Option to create a non-nullable reference type.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Iridium")]
 [assembly: AssemblyProduct("Types")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
